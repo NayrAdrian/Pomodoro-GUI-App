2709b2e5c7028685272caa735d553ebd7f3b2a44
Ryan Adrian <NayrAdrian24@gmail.com>
2024-06-18 19:02:42 +0800

    Added Timer label, Start and Reset Button and Checkmark

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -18,15 +19,38 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+def button_start():
+    pass
+
+
+def button_reset():
+    pass
+
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+#Timer Label
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 38, "bold"))
+title_label.grid(column=1, row=0)
+
+#Tomato Image and Timer
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=tomato_img)
 canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+#Start Button
+start_button = Button(text="Start", command=button_start)
+start_button.grid(column=0, row=3)
+
+#Reset Button
+reset_button = Button(text="Reset", command=button_reset)
+reset_button.grid(column=2, row=3)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
