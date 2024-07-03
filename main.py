205bb0617cd6a76124f4e2b9d0e174466ba0cda7
Ryan Adrian <NayrAdrian24@gmail.com>
2024-07-03 16:16:01 +0800

    v1

--- main.py
+++ main.py
@@ -11,9 +11,19 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", fg=GREEN)
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
@@ -28,12 +38,13 @@ def start_timer():
     if reps % 8 == 0:
         count_down(long_break_sec)
         title_label.config(text="Long Break", fg=RED)
+
     elif reps % 2 == 0:
         count_down(short_break_sec)
         title_label.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
-        title_label.config(text="Work", fg=GREEN)
+        title_label.config(text="FOCUS", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -48,16 +59,18 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
-def button_reset():
-    pass
 
 
 window = Tk()
@@ -80,10 +93,10 @@ start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=3)
 
 # Reset Button
-reset_button = Button(text="Reset", command=button_reset)
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(column=2, row=3)
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
